d52bf36daeb58f3d9dbaac2e5235af3d84755580
Batsaikhan <batsaikhan.dash@novelsoft.mn>
2023-09-04 22:17:32 +0800

    header update

---
+++ components/Header.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import Image from "next/image";
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import Avatar from "react-avatar";
+
+
+function Header() {
+  return (
+    <header>
+      <div className="flex flex-col md:flex-row items-center p-5 bg-[#c9c8b6]/10 shadow-md rounded-lg">
+
+        {/*Gradient Back color*/}
+      
+
+        {/*logo  */}
+        <Image
+            src="/assets/logo.svg"
+            alt="Bello Logo"
+            width={300}
+            height={100}
+            className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
+        />
+
+        {/* Search box and Avatar */}
+        <div className="flex items-center space-x-5 flex-1 justify-end w-full">
+           <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-sm flex-1 md:flex-initial">
+            <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+            <input type="text" placeholder="Search" className="flex-1 outline-none"/>
+            <button hidden>Search</button>
+           </form>
+
+          <Avatar name="Batji Dashdorj" size="50" color="#1c90bf" round />
+        </div>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
