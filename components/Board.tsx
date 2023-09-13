d25c45a68a5d6ed96cbd99cb786e288e47343bf5
Batji <Batji@DESKTOP-EJI39KG>
2023-09-13 17:23:35 +0800

    update

--- components/Board.tsx
+++ components/Board.tsx
@@ -3,11 +3,7 @@ import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 function Board() {
   return (
-    <DragDropContext>
-        <Droppable droppableId="board" direction="horizontal" type="column">
-            {(provided) => <div></div>}
-        </Droppable>    
-    </DragDropContext>
+    
   )
 }
 
